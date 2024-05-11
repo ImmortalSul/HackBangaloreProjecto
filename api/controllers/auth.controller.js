b3bd6bc2424759766f9c2d94a85ffdebf6d2fcaf
ImmortalSul <157292013+ImmortalSul@users.noreply.github.com>
2024-05-12 02:25:05 +0530

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,11 +6,13 @@ export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    
+    // HASH THE PASSWORD
 
-    const hashedPassword = await bcrypt.hash(password, 10); 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     console.log(hashedPassword);
 
+    // CREATE A NEW USER AND SAVE TO DB
     const newUser = await prisma.user.create({
       data: {
         username,
@@ -32,7 +34,7 @@ export const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    
+    // CHECK IF THE USER EXISTS
 
     const user = await prisma.user.findUnique({
       where: { username },
@@ -40,15 +42,16 @@ export const login = async (req, res) => {
 
     if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
 
-   
+    // CHECK IF THE PASSWORD IS CORRECT
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid)
       return res.status(400).json({ message: "Invalid Credentials!" });
 
-   
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
 
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
     const age = 1000 * 60 * 60 * 24 * 7;
 
     const token = jwt.sign(
@@ -65,7 +68,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        //secure:true,  remember to turn on in production build lol
+        // secure:true,
         maxAge: age,
       })
       .status(200)
