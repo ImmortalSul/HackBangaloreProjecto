babcfc2916d09673e1fed6c543ac3cb3e1e93bf5
ImmortalSul <157292013+ImmortalSul@users.noreply.github.com>
2024-05-12 07:20:04 +0530

    commits

--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import{
-    addMessage
-} from "../controllers/message.controllers.js";
+import {
+  addMessage
+} from "../controllers/message.controller.js";
+import {verifyToken} from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
 
 router.post("/:chatId", verifyToken, addMessage);
 
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {verifyToken} from "../middleware/verifyToken.js";
-import { addPost, deletePost, getPost, getPosts, updatePost} from ",,/controllers/post.controller.js";
+import { addPost, deletePost, getPost, getPosts, updatePost} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
